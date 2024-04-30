f7418b342ee25d012691c38c66c74c26930dc968
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-04-30 16:14:59 +0200

    Create example.java

---
+++ example.java
@@ -0,0 +1,8 @@
+/** 
+ * example of a java program for the Apasoft Training Jenkins course
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("This is an example of a java program");
+  }
+}
