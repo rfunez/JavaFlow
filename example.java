c82be6007bd0060aa1f813d145bf41731705b935
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-05-01 02:41:30 +0200

    Delete example.java

--- example.java
+++
@@ -1,8 +0,0 @@
-/** 
- * example of a java program for the Apasoft Training Jenkins course
- */
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("This is an example of a java program");
-  }
-}
