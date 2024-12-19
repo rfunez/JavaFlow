9754515ed3bcc32abc9f6f94e21869733aa66ad7
rfunez <rfunez1972@gmail.com>
2024-12-19 19:36:39 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -4,7 +4,7 @@
 
 public class Example {
   public static void main(String[] args) {
-    System.out.println("This is an example of a java program");
+    System.out.println("This is an example of a java program on Git Flow");
   }
 }
 
